9d547cc93bc6169f8eefdf8830a7cdbdf13835d1
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-08-04 14:04:52 -0400

    q3

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,22 @@
 import React from "react";
-
+import { SeatContext } from "./SeatContext";
 import GlobalStyles from "./GlobalStyles";
-
 function App() {
+  const {
+    state: { numOfRows },
+    actions: { recieveSeatInfoFromServer },
+  } = React.useContext(SeatContext);
+  React.useEffect(() => {
+    fetch("/api/seat-availibility")
+      .then((res) => res.json())
+      .then((data) => recieveSeatInfoFromServer(data));
+  }, []);
+  console.log(SeatContext);
   return (
     <>
       <GlobalStyles />
-      Daje Roma
+      This venue has this number of rows: {numOfRows}
     </>
   );
 }
-
 export default App;
--- src/components/SeatContext.js
+++ src/components/SeatContext.js
@@ -11,7 +11,20 @@ const initialState = {
 };
 
 function reducer(state, action) {
-  // TODO
+  console.log(action);
+  switch (action.type) {
+    case "receive-seat-info-from-server": {
+      return {
+        ...state,
+        hasLoaded: true,
+        seats: action.seats,
+        numOfRows: action.numOfRows,
+        seatsPerRow: action.seatsPerRow,
+      };
+    }
+    default:
+      throw new Error(`Unrecognized action: ${action.type}`);
+  }
 }
 
 export const SeatProvider = ({ children }) => {
